a60631e8a73fe01c211813bb3bc4995a057b705c
dev-indra01 <dev.indra.dev@gmail.com>
2024-08-25 23:28:58 +0530

    update 8

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,16 +8,17 @@ router.route("/register")
     .post(upload.fields([{ name: "avatar", maxCount: 1 }, { name: "coverImage", maxCount: 1 }]), registerUser)
 
 router.route("/login").post(loginUser)
-router.route("/").get(home)
+// router.route("/").get(home)
+router.get("/", function (req, res) { res.send("home page"); });
 
 //secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
